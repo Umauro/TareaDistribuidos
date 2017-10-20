7a956ea518e20262118c2219a568654bb19276ef
Umauro <felipe.vega.14@sansano.usm.cl>
2017-10-20 17:23:58 -0300

    add pedirIp function

--- cliente.java
+++ cliente.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.util.*;
 
 public class cliente {
+  private InetAddress addressCentral = new InetAddress();
 
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
@@ -12,13 +13,13 @@ public class cliente {
 
         // get a **DatagramSocket**
         DatagramSocket socket = new DatagramSocket();
-
         // send request
         byte[] buf = new byte[256];
         String mensaje = "Hola";
         buf = mensaje.getBytes();
-        InetAddress address = InetAddress.getByName(args[0]);
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+        pedirIp();
+        //InetAddress address = InetAddress.getByName(args[0]);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, addressCentral, 4445);
         socket.send(packet);
 
         // get response
@@ -30,5 +31,13 @@ public class cliente {
         System.out.println("Quote of the Moment: " + received);
 
         socket.close();
+    }
+
+    public void pedirIp(){
+      String entradaTeclado = "";
+      Scanner entrada = new Scanner(System.in);
+      System.out.println("Ingrese IP servidor central");
+      entradaTeclado = entrada.nextLine();
+      anddresCentral.getByName(entradaTeclado)
     }
 }
