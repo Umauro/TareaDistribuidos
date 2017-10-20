d85d3f78ca728ba21f952bed816e660fbd55979d
Umauro <felipe.vega.14@sansano.usm.cl>
2017-10-20 17:57:01 -0300

    fix errors

--- cliente.java
+++ cliente.java
@@ -3,7 +3,8 @@ import java.net.*;
 import java.util.*;
 
 public class cliente {
-  private InetAddress addressCentral = new InetAddress();
+    private static InetAddress addressCentral;
+    private static int puerto;
 
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
@@ -11,15 +12,28 @@ public class cliente {
              return;
         }
 
+
         // get a **DatagramSocket**
         DatagramSocket socket = new DatagramSocket();
         // send request
         byte[] buf = new byte[256];
         String mensaje = "Hola";
         buf = mensaje.getBytes();
-        pedirIp();
+        //pedirIp();
         //InetAddress address = InetAddress.getByName(args[0]);
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, addressCentral, 4445);
+
+        String entradaTeclado = " ";
+        Scanner entrada = new Scanner(System.in);
+
+        //Pedir Datos del Servidor Central
+        System.out.println("Ingrese IP servidor central: ");
+        entradaTeclado = entrada.nextLine();
+        System.out.println("Ingrese Puerto servidor central: ");
+        puerto = entrada.nextInt();
+
+        addressCentral = InetAddress.getByName(entradaTeclado);
+        
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, addressCentral, puerto);
         socket.send(packet);
 
         // get response
@@ -31,13 +45,5 @@ public class cliente {
         System.out.println("Quote of the Moment: " + received);
 
         socket.close();
-    }
-
-    public void pedirIp(){
-      String entradaTeclado = "";
-      Scanner entrada = new Scanner(System.in);
-      System.out.println("Ingrese IP servidor central");
-      entradaTeclado = entrada.nextLine();
-      anddresCentral.getByName(entradaTeclado)
     }
 }
